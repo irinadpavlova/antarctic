af642cbf5cb0b383c64369eeeadd2a3dea76a471
irinadpavlova <irinadpavlova@gmail.com>
2020-09-23 13:57:10 +0300

    исправляет баги

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var body = document.querySelector('body');
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -9,9 +10,11 @@
   navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
+      body.classList.add('body-lock');
       navMain.classList.add('main-nav--opened');
     } else {
       navMain.classList.add('main-nav--closed');
+      body.classList.remove('body-lock');
       navMain.classList.remove('main-nav--opened');
     }
   });
